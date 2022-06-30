60f90f6e84c732dae27484ccfed713cf2c0759a3
Md. Abdullah- Al Firoz <firozcse2020@gmail.com>
2022-06-30 10:35:57 +0600

    file transfer to React HOC

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
+function App() {
+    return (
+        <div className="app">
+            <ClickCounter />
+            <HoverCounter />
+        </div>
+    );
+}
+
+export default App;
